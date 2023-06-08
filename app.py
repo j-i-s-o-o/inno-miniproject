ecc09417c6478771b038cc4404666075ddad107f
j-i-s-o-o <135205241+j-i-s-o-o@users.noreply.github.com>
2023-06-08 15:47:46 +0900

    Update app.py
    방명록 삭제

--- app.py
+++ app.py
@@ -14,18 +14,28 @@ def guestbook_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
 
+    all_comments = list(db.cheer.find({}, {'_id': False}))
+    count = len(all_comments) + 1
     doc = {
+        'num':count, 
         'name':name_receive,
-        'comment':comment_receive
+        'comment':comment_receive,
     }
     db.cheer.insert_one(doc)
-    # db명 수정 할 것
     return jsonify({'msg': '저장 완료!'})
 
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
     all_comments = list(db.cheer.find({},{'_id':False}))
     return jsonify({'result': all_comments})
+
+@app.route("/guestbook/delete", methods=["POST"])
+def guestbook_delete():
+    num_receive = request.form['num_give']
+
+    db.cheer.delete_one({'num':int(num_receive)})
+
+    return jsonify({'msg': '삭제 완료!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)

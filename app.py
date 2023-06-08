7d1fd0f97215bc9d82137a57ed4699954b71a28b
j-i-s-o-o <135205241+j-i-s-o-o@users.noreply.github.com>
2023-06-08 19:42:35 +0900

    Update app.py
    삭제 기능 추가

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = client.dbsparta
 def home():
    return render_template('index.html')
 
-@app.route("/guestbook", methods=["POST"])
+@app.route("/questionbook", methods=["POST"])
 def guestbook_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
@@ -22,20 +22,29 @@ def guestbook_post():
         'comment':comment_receive,
     }
     db.cheer.insert_one(doc)
-    return jsonify({'msg': '저장 완료!'})
+    return jsonify({'msg': '질문 등록 완료!'})
 
-@app.route("/guestbook", methods=["GET"])
-def guestbook_get():
+@app.route("/questionbook", methods=["GET"])
+def questionbook_get():
     all_comments = list(db.cheer.find({},{'_id':False}))
     return jsonify({'result': all_comments})
 
-@app.route("/guestbook/delete", methods=["POST"])
-def guestbook_delete():
+# @app.route("/questionbook/modify", methods=["POST"])
+# def questionbook_modify():
+#     num_receive = request.form['num_give']
+#     comment_receive = request.form['comment_give']
+
+#     db.cheer.update_one({'num':int(num_receive)},{'$set':{'comment':comment_receive}})
+
+#     return jsonify({'msg': '질문 수정 완료!'})
+
+@app.route("/questionbook/delete", methods=["POST"])
+def questionbook_delete():
     num_receive = request.form['num_give']
 
     db.cheer.delete_one({'num':int(num_receive)})
 
-    return jsonify({'msg': '삭제 완료!'})
+    return jsonify({'msg': '질문 삭제 완료!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
